9c310605ea34628fe448584cc1cc4576adaa5111
lucasjose256 <79601444+lucasjose256@users.noreply.github.com>
2024-08-07 19:21:42 -0300

    side-bar-menu

---
+++ wwwroot/js/sidebar.js
@@ -0,0 +1,12 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    var sidebarMenu = document.getElementById('sidebar-menu');
+    var menuItems = sidebarMenu.getElementsByClassName('nav-link');
+
+    for (var i = 0; i < menuItems.length; i++) {
+        menuItems[i].addEventListener('click', function () {
+            var current = sidebarMenu.getElementsByClassName('active');
+            current[0].classList.remove('active');
+            this.classList.add('active');
+        });
+    }
+});
